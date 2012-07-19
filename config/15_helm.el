70042c496eba354d1b3db3f50b67c62d7258b204
Wataru MIYAGUNI <gonngo@gmail.com>
2012-07-19 22:03:34 +0900

    helm-git-project をコピペ from http://d.hatena.ne.jp/syohex/20120718/1342620627

--- config/15_helm.el
+++ config/15_helm.el
@@ -1,5 +1,6 @@
 (require 'helm-config)
 (require 'helm-gtags)
+(require 'helm-files)
 
 (defun helm-my-default()
   (interactive)
@@ -25,3 +26,39 @@
   (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
   (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
   (local-set-key (kbd "C-t") 'helm-gtags-pop-stack))
+
+;; List files in git repos
+(defun helm-c-sources-git-project-for (pwd)
+  (loop for elt in
+        '(("Modified files" . "--modified")
+          ("Untracked files" . "--others --exclude-standard")
+          ("All controlled files in this project" . nil))
+        for title  = (format "%s (%s)" (car elt) pwd)
+        for option = (cdr elt)
+        for cmd    = (format "git ls-files %s" (or option ""))
+        collect
+        `((name . ,title)
+          (init . (lambda ()
+                    (unless (and ,option (helm-candidate-buffer))
+                      (with-current-buffer (helm-candidate-buffer 'global)
+                        (call-process-shell-command ,cmd nil t nil)))))
+          (candidates-in-buffer)
+          (type . file))))
+
+(defun helm-git-project-topdir ()
+  (file-name-as-directory
+   (replace-regexp-in-string
+    "\n" ""
+    (shell-command-to-string "git rev-parse --show-toplevel"))))
+
+(defun helm-git-project ()
+  (interactive)
+  (let ((topdir (helm-git-project-topdir)))
+    (unless (file-directory-p topdir)
+      (error "I'm not in Git Repository!!"))
+    (let* ((default-directory topdir)
+           (sources (helm-c-sources-git-project-for default-directory)))
+      (helm-other-buffer sources
+                         (format "*helm git project in %s*" default-directory)))))
+
+(define-key global-map (kbd "C-;") 'helm-git-project)
