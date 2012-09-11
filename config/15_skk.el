a200719013dba4efab5c7a8182ec7be5450f9ace
Wataru MIYAGUNI <gonngo@gmail.com>
2012-09-11 10:50:08 +0900

    add el-get recipe "togetter-el"

--- config/15_skk.el
+++ config/15_skk.el
@@ -3,6 +3,6 @@
 
 (setq skk-server-portnum 1178)
 (setq skk-server-host "localhost")
-
+(setq skk-show-inline 'vertical)
 (global-set-key (kbd "C-x j") 'skk-mode)
 
--- config/50_smartrep.el
+++ config/50_smartrep.el
@@ -1,6 +1,7 @@
 (require 'smartrep)
 
 ;; for OrgMode
-(smartrep-define-key
-    org-mode-map "C-c" '(("C-n" . (outline-next-visible-heading 1))
-                         ("C-p" . (outline-previous-visible-heading 1))))
+(defun-add-hook 'org-mode-hook
+  (smartrep-define-key
+      org-mode-map "C-c" '(("C-n" . (outline-next-visible-heading 1))
+                           ("C-p" . (outline-previous-visible-heading 1)))))
