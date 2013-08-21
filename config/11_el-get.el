21c56dfbd8d638c7547e4197965bc41e5ffc0f43
Wataru MIYAGUNI <gonngo@gmail.com>
2013-08-21 13:17:04 +0900

    el-get は本家ではなく、wget 可能な HKey/el-get に変更した

--- config/11_el-get.el
+++ config/11_el-get.el
@@ -1,15 +1,17 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/el-get/el-get"))
 
 (unless (require 'el-get nil t)
-  (with-current-buffer
-      (find-file-noselect "~/.emacs.d/vendor/el-get-install.el")
-    (let (el-get-master-branch
-          (el-get-emacswiki-elisp-file-list-url
-           "https://raw.github.com/gongo/elfactory/master/vendor/el-get-emacswiki-retrieve-package-list.txt"))
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+  (let ((el-get-git-install-url "https://github.com/HKey/el-get.git"))
+    (with-current-buffer
+	(find-file-noselect "~/.emacs.d/vendor/el-get-install.el")
+      (let (el-get-master-branch
+	    (el-get-emacswiki-elisp-file-list-url
+	     "https://raw.github.com/gongo/elfactory/master/vendor/el-get-emacswiki-retrieve-package-list.txt"))
+	(goto-char (point-max))
+	(eval-print-last-sexp)))))
 
 (add-to-list 'el-get-recipe-path
              (expand-file-name "~/.emacs.d/el-get-recipes"))
 
+(setq el-get-use-wget t)
 (el-get 'sync)
