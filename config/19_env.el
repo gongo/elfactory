02a67723b930b1e079ceed1e06d6925742591476
Wataru MIYAGUNI <gonngo@gmail.com>
2014-03-14 14:41:33 +0900

    homebrew path をほげほげ

--- config/19_env.el
+++ config/19_env.el
@@ -2,10 +2,10 @@
 (setenv "SHELL"
         (cond
          (linux-p "/usr/bin/zsh")
-         (t "/Users/gongo/.homebrew/bin/zsh")))
+         (t (concat (getenv "HOMEBREW_ROOT") "/bin/zsh"))))
 
 (when (memq window-system '(mac ns x))
   (let* ((libenv (cond (darwin-p "DYLD_FALLBACK_LIBRARY_PATH")
                        (linux-p  "LD_LIBRARY_PATH")))
-         (envs `("PATH" "GOROOT" "GOPATH" "RBENV_ROOT" ,libenv)))
+         (envs `("PATH" "GOROOT" "GOPATH" "RBENV_ROOT" "HOMEBREW_ROOT" ,libenv)))
     (exec-path-from-shell-copy-envs envs)))
