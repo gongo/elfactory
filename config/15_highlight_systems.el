1a40376b762a7bddaba616a69be22f15748c056c
Wataru MIYAGUNI <gonngo@gmail.com>
2014-08-21 09:37:12 +0900

    ハイライト系は一箇所にあつめる事にしました

--- config/15_highlight_systems.el
+++ config/15_highlight_systems.el
@@ -1,6 +1,7 @@
 (require 'crosshairs)
-
 (set-face-background hl-line-face "gray13")
 (set-face-background col-highlight-face "gray13")
-
 (crosshairs-mode 1)
+
+(require 'volatile-highlights)
+(volatile-highlights-mode t)
