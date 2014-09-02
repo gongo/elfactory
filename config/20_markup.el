c159fc9b88360b31a8c41505c7f7481ee2cf690f
Wataru MIYAGUNI <gonngo@gmail.com>
2014-09-02 09:59:09 +0900

    Add web-mode config

--- config/20_markup.el
+++ config/20_markup.el
@@ -1,3 +1,6 @@
+;;
+;; emmet
+;;--------------------
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook  'emmet-mode)
@@ -9,3 +12,18 @@
   '(define-key emmet-mode-keymap (kbd "C-j") nil))
 
 (define-key emmet-mode-keymap (kbd "C-<tab>") 'emmet-expand-line)
+
+;;
+;; web-mode
+;;--------------------
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tpl\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ihtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun-add-hook 'web-mode-hook
+  (setq web-mode-html-offset   4)
+  (setq web-mode-css-offset    4)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset    4)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4))
