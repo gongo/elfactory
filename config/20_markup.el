4ce144aac9e66a7412937f1fb6c9bc466027aeba
Wataru MIYAGUNI <gonngo@gmail.com>
2014-10-20 21:54:33 +0900

    Remove require sentence

--- config/20_markup.el
+++ config/20_markup.el
@@ -1,29 +1,28 @@
 ;;
 ;; emmet
 ;;--------------------
-(require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook  'emmet-mode)
+(add-hook 'web-mode-hook  'emmet-mode)
 
-(defun-add-hook 'emmet-mode-hook
-  (setq emmet-indentation 2))
-
-(eval-after-load "emmet-mode"
-  '(define-key emmet-mode-keymap (kbd "C-j") nil))
-
-(define-key emmet-mode-keymap (kbd "C-<tab>") 'emmet-expand-line)
+(eval-after-load 'emmet-mode
+  '(progn
+     (defun-add-hook 'emmet-mode-hook
+       (setq emmet-indentation 2))
+     (define-key emmet-mode-keymap (kbd "C-j") nil)
+     (define-key emmet-mode-keymap (kbd "C-<tab>") 'emmet-expand-line)))
 
 ;;
 ;; web-mode
 ;;--------------------
-(require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tpl\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ihtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(defun-add-hook 'web-mode-hook
-  (setq web-mode-html-offset   4)
-  (setq web-mode-css-offset    4)
-  (setq web-mode-script-offset 4)
-  (setq web-mode-php-offset    4)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 4))
+
+(eval-after-load 'web-mode
+  '(defun-add-hook 'web-mode-hook
+     (setq web-mode-markup-indent-offset 4)
+     (setq web-mode-css-indent-offset    4)
+     (setq web-mode-code-indent-offset   4)
+     (setq indent-tabs-mode nil)
+     (setq tab-width 4)))
