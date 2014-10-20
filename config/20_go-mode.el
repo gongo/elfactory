cd8bd601d19e33418422ecf4125c4112449e3292
Wataru MIYAGUNI <gonngo@gmail.com>
2014-10-20 22:13:43 +0900

    Load go-autocomplete after load go-mode

--- config/20_go-mode.el
+++ config/20_go-mode.el
@@ -1,4 +1,3 @@
-(require 'go-autocomplete)
 (require 'auto-complete-config)
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
@@ -7,10 +6,13 @@
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-(defun-add-hook 'go-mode-hook
-  (setq c-basic-offset 4)
-  (setq indent-tabs-mode t)
-  (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-  (local-set-key (kbd "C-c i") 'go-goto-imports)
-  (local-set-key (kbd "C-c d") 'godoc))
+(eval-after-load 'go-mode
+  '(progn
+     (require 'go-autocomplete)
+     (defun-add-hook 'go-mode-hook
+       (setq c-basic-offset 4)
+       (setq indent-tabs-mode t)
+       (local-set-key (kbd "M-.") 'godef-jump)
+       (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+       (local-set-key (kbd "C-c i") 'go-goto-imports)
+       (local-set-key (kbd "C-c d") 'godoc))))
