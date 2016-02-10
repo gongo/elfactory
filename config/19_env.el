4147760e94c7239bb1e6bfd9f71d2e9076a35cce
Wataru MIYAGUNI <gonngo@gmail.com>
2016-02-10 09:54:07 +0900

    http://gongo.hatenablog.com/entry/2016/02/10/092956

--- config/19_env.el
+++ config/19_env.el
@@ -7,5 +7,5 @@
 (when (memq window-system '(mac ns x))
   (let* ((libenv (cond (darwin-p "DYLD_FALLBACK_LIBRARY_PATH")
                        (linux-p  "LD_LIBRARY_PATH")))
-         (envs `("PATH" "GOROOT" "GOPATH" "RBENV_ROOT" "HOMEBREW_ROOT" ,libenv)))
+         (envs `("PATH" "GOROOT" "GOPATH" "RBENV_ROOT" "HOMEBREW_ROOT" "LC_ALL" ,libenv)))
     (exec-path-from-shell-copy-envs envs)))
